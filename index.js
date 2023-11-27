dbe7f56610bebae5720a6a8902d4b05f6b3f1b53
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-11-27 20:03:06 +0100

    13

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
 import express from "express";
 import 'dotenv/config';
-// import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
 const { Pool } = pg;
 const pool = new Pool({
-  connectionString: "postgres://default:CEjehzq83vga@ep-lively-night-27832602-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 })
-
-const password = process.env.POSTGRES_PASSWORD;
-const username = process.env.POSTGRES_USER
-const databaseName = process.env.POSTGRES_DATABASE;
-const host = process.env.POSTGRES_HOST;
-
-// const sequelize = new Sequelize( databaseName, username, password, {
-//   host,
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       rejectUnauthorized: false, 
-//     },
-//   },
-//   logging: false,
-// });
-
-
-// const database = process.env.POSTGRES_URL + '?sslmode=require';
-// const sequelize = new Sequelize(database);
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connected to the database.');
-//   })
-//   .catch((error) => {
-//     console.error('Unable to connect to the database:', error);
-//   });
-
 
 const app = express();
 
