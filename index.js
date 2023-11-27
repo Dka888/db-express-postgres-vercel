0e4d32a5bee08111ded62d2eac1fee6daeb002e2
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-11-28 00:23:28 +0100

    cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import 'dotenv/config';
 import { router as UserRouter} from "./app/routes/user-routes.js";
 import {router as NoteRouter} from './app/routes/note-routes.js';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/users', UserRouter);
 app.use('/notes', NoteRouter);
