46de0b20ab0681e4482ba261eeee5cabd5002c32
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-11-27 17:56:36 +0100

    5

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const username = process.env.POSTGRES_USER
 const databaseName = process.env.POSTGRES_DATABASE;
 const host = process.env.POSTGRES_HOST;
 
-const sequelize = new Sequelize( databaseName, username, password, {
-  host,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false, 
-    },
-  },
-  logging: false,
-});
+// const sequelize = new Sequelize( databaseName, username, password, {
+//   host,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false, 
+//     },
+//   },
+//   logging: false,
+// });
+
+
+const database = process.env.POSTGRES_URL + '?sslmode=require';
+const sequelize = new Sequelize(database);
 
 sequelize
   .authenticate()
